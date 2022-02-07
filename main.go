b523969678b32479a1da33287f0a37957538e68b
bdemirpolat <o.b.demirpolat@gmail.com>
2022-02-07 16:57:19 +0300

    workflow

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("hi"))
+		w.Write([]byte("hi2"))
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
